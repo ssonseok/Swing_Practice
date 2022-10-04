0fa117134d8d7df66b704072e5ee5213074b2374
Seokhyeon Son <tjrgus1849@naver.com>
2022-10-04 19:02:49 +0900

    commit message

--- src/swing/SwingPractice.java
+++ src/swing/SwingPractice.java
@@ -10,22 +10,21 @@ import javax.swing.JPanel;
 
 public class SwingPractice extends JFrame {
 	public SwingPractice() {
-		JPanel pan1 = new JPanel();	//pan1이라는 패널 생성
-		JButton btn1 = new JButton("Click here!");	//btn1이라는 버튼 생성
+		JPanel pan1 = new JPanel();	
+		JButton btn1 = new JButton("Click here!");
+		add(pan1);
+		pan1.add(btn1);
 		
-		add(pan1);	//pan1이라는 패널 생성
-		pan1.add(btn1);	//btn1을 추가
-		
-		btn1.addActionListener(new ActionListener() {	//btn1을 눌렀을 때 효과추가
+		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "You have been clicked!");	//화면 가운데에 메시지출력
+				JOptionPane.showMessageDialog(null, "You have been clicked");
 			}
 		});
 		
-		setVisible(true);	//창을 눈에 보이도록 함
-		setSize(640, 480);	//사이즈는 640*480
-		setResizable(false);	//사이즈 재조정 불가능
-		setLocationRelativeTo(null);	//창이 가운데에 뜨도록 함
-		setDefaultCloseOperation(EXIT_ON_CLOSE);	//창을 끄면 프로그램을 종료
+		setVisible(true);
+		setSize(640, 480);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
